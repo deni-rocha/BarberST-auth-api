fc1df8573d9e12f15300677a90a202a9a5a33f5a
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-10 00:37:48 -0300

    refatoração

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,12 @@
 import express = require("express")
 import { Router } from "express"
-import dotenv = require("dotenv")
+
 import cors = require("cors")
-import mongoose from "mongoose"
 import authRouter from "./routes/auth"
 import userRouter from "./routes/user"
+import loaders from "./loaders"
 
 // configurações
-dotenv.config()
 const app = express()
 const router = Router()
 
@@ -15,29 +14,14 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
-
 // rota de autenticação
 app.use("/api/auth", authRouter)
 
 // rota privada
 app.use("/api/user", userRouter)
 
-// connect database
-void (async () => {
-  try {
-    mongoose.set("strictQuery", true)
-
-    const response = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.sq7oujl.mongodb.net/?retryWrites=true&w=majority`
-    )
-
-    console.log("deu bom")
-  } catch (error) {
-    console.log("deu ruim")
-  }
-})()
+// conectar database
+loaders()
 
 const porta = process.env.PORT || 4000
 
