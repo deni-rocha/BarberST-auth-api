a7cb16986a7b212532962ac41616d55aea6e56b6
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-10 00:38:05 -0300

    alteração no schema

--- api/models/User.ts
+++ api/models/User.ts
@@ -4,12 +4,14 @@ interface IUser {
   name: string
   password: string
   email: string
+  perfil: string
 }
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true }
+  email: { type: String, required: true },
+  perfil: { type: String, default: "comum" }
 })
 
 const User = model<IUser>("User", userSchema)
